c0b6b6ff7442540129aaa0ee95b456755f066b40
Prabhav Negi <prabhav511@mail.com>
2021-05-16 23:15:20 +0530

    sdfd

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var path = require('path')
 
 var app = express()
 app.use(express.static("public"));
-port = process.env.port || 5000
+port = process.env.port || 8080
 app.get('/', function(req, res) {
     res.sendFile('/index.html');
   });
